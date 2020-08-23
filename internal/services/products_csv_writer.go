ebd2382242f0cda953b5c2c8857d8750caab7847
Kishan B <bkishan@thoughtworks.com>
2020-08-23 08:59:52 +0530

    Refactor productsCSVWriterService

--- internal/services/products_csv_writer.go
+++ internal/services/products_csv_writer.go
@@ -27,6 +27,18 @@ func NewProductsCSVWriterService(productCSVConversionService ProductCSVConversio
 }
 
 func (service *productsCSVWriterService) DownloadAllProducts(out io.Writer) error {
+	return service.downloadAllProductsUpdatingProgressStateIfRequired(out, nil)
+}
+
+func (service *productsCSVWriterService) DownloadAllProductsUpdatingProgressState(out io.Writer, progressState chan<- progress.State) error {
+	return service.downloadAllProductsUpdatingProgressStateIfRequired(out, progressState)
+}
+
+func (service *productsCSVWriterService) downloadAllProductsUpdatingProgressStateIfRequired(out io.Writer, progressState chan<- progress.State) error {
+	currentProgressState := progress.State{
+		NoOfProductsDownloaded:     0,
+		NoOfProductsConvertedAsCSV: 0,
+	}
 	csvWriter := make(chan interface{}, 1000)
 	operation, _ := errgroup.WithContext(context.Background())
 	operation.Go(func() error {
@@ -37,42 +49,19 @@ func (service *productsCSVWriterService) DownloadAllProducts(out io.Writer) erro
 		return service.productsRetrievalService.RetrieveAllProducts(products)
 	})
 	for product := range products {
+		if progressState != nil {
+			currentProgressState.NoOfProductsDownloaded++
+			progressState <- currentProgressState
+		}
 		productCSVs := service.productCSVConversionService.ConvertToCSVFormat(product)
+		if progressState != nil {
+			currentProgressState.NoOfProductsConvertedAsCSV++
+			progressState <- currentProgressState
+		}
 		for _, productCSV := range productCSVs {
 			csvWriter <- productCSV
 		}
 	}
 	close(csvWriter)
 	return operation.Wait()
-}
-
-func (service *productsCSVWriterService) DownloadAllProductsUpdatingProgressState(out io.Writer, progressState chan<- progress.State) error {
-	currentProgressState := progress.State{
-		NoOfProductsDownloaded:     0,
-		NoOfProductsConvertedAsCSV: 0,
-	}
-	csvWriter := make(chan interface{}, 1000)
-	errors := make(chan error, 1)
-	go func() {
-		errors <- gocsv.MarshalChan(csvWriter, gocsv.DefaultCSVWriter(out))
-		close(errors)
-	}()
-	products := make(chan shopify.Product, 1000)
-	go func() {
-		errors <- service.productsRetrievalService.RetrieveAllProducts(products)
-		close(errors)
-	}()
-	for product := range products {
-		currentProgressState.NoOfProductsDownloaded++
-		progressState <- currentProgressState
-		productCSVs := service.productCSVConversionService.ConvertToCSVFormat(product)
-		currentProgressState.NoOfProductsConvertedAsCSV++
-		progressState <- currentProgressState
-		for _, productCSV := range productCSVs {
-			csvWriter <- productCSV
-		}
-	}
-	close(progressState)
-	close(csvWriter)
-	return <-errors
 }
